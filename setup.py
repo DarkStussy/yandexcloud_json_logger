4d726c84f1dd9f2d311070304ee7cd87758d67fa
Stanislav <chornij.stas@gmail.com>
2023-05-29 21:24:05 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='yandexcloud_json_logger',
+    version='0.0.1',
+    author='darkstussy',
+    author_email='chornij.stas@gmail.com',
+    description='Cloud functions json logger',
+    url='https://github.com/Darkstussy/yandexcloud_json_logger',
+    project_urls={
+        "Bug Tracker": "https://github.com/Darkstussy/yandexcloud_json_logger/issues"
+    },
+    license='MIT',
+    packages=['yandexcloud_json_logger'],
+    install_requires=['python-json-logger==2.0.7'],
+)
